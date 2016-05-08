7f6f84d8f802d1e29557ce9aa8ae23fefabcbdd9
meggsmith <meggie@google.com>
2016-05-08 12:47:58 -0400

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,15 @@
 'use strict';
+function createArray() {
+  var createArray = ["snickers", "hundred grand"
+   "kitkat", "skittles"];
+  return createArray;
+}
+
+function addElementToArray(){
+  addElementToArray.push("A Christmas Story");
+  return addElementToArray;
+}
+
+function accessThirdElementFromArray(){
+  addElementToArray
+}
